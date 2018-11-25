12fb168623798186413eeeae359d52c8c120812b
Negar <n.jamalifard@gmail.com>
2018-11-26 01:50:40 +0330

    remove junk from app component

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,10 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>GitHub</span>
+        <span class="font-weight-light">Notifier</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>open_in_new</v-icon>
-      </v-btn>
     </v-toolbar>
 
     <v-content>
