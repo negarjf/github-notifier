f1c2d69985a954e747188d5a20c6e78a4ef6b6e4
Negar <n.jamalifard@gmail.com>
2018-11-30 01:13:35 +0330

    add notifications to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,13 @@
 <template>
-
+    <notifications></notifications>
 </template>
 
 <script>
+    import Notifications from '@/components/Notifications';
 
     export default {
         components: {
-
+            Notifications
         },
 
     }
