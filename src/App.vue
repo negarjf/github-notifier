856895d84630b05052b03f7bfb0ae31f26fd5dca
Negar <n.jamalifard@gmail.com>
2018-11-30 00:34:04 +0330

    move logout event listener to app component

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,60 @@
 <template>
-  <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>GitHub</span>
-        <span class="font-weight-light">Notifier</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-    </v-toolbar>
+    <v-app>
+        <v-toolbar app>
+            <v-toolbar-title class="headline text-uppercase">
+                <span>GitHub</span>
+                <span class="font-weight-light">Notifier</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+        </v-toolbar>
 
-    <v-content>
-      <router-view/>
-        <v-bottom-nav
-                :active.sync="bottomNav"
-                color="primary"
-                :value="true"
-                app
-                fixed
-                shift
-        >
-            <v-btn dark>
-                <span>Video</span>
-                <v-icon>ondemand_video</v-icon>
-            </v-btn>
+        <v-content>
+            <router-view/>
+            <v-bottom-nav
+                    :active.sync="bottomNav"
+                    color="primary"
+                    :value="true"
+                    app
+                    fixed
+                    shift
+            >
+                <v-btn dark>
+                    <span>Video</span>
+                    <v-icon>ondemand_video</v-icon>
+                </v-btn>
 
-            <v-btn dark>
-                <span>Music</span>
-                <v-icon>music_note</v-icon>
-            </v-btn>
+                <v-btn dark>
+                    <span>Music</span>
+                    <v-icon>music_note</v-icon>
+                </v-btn>
 
-            <v-btn dark>
-                <span>Book</span>
-                <v-icon>book</v-icon>
-            </v-btn>
+                <v-btn dark>
+                    <span>Book</span>
+                    <v-icon>book</v-icon>
+                </v-btn>
 
-            <v-btn dark>
-                <span>Image</span>
-                <v-icon>image</v-icon>
-            </v-btn>
-        </v-bottom-nav>
-    </v-content>
-  </v-app>
+                <v-btn dark>
+                    <span>Image</span>
+                    <v-icon>image</v-icon>
+                </v-btn>
+            </v-bottom-nav>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
 
-export default {
-  name: 'App',
-  data () {
-    return {
-      //
+    export default {
+        name: 'App',
+        data() {
+            return {
+                //
+            }
+        },
+        created() {
+            this.$electron.ipcRenderer.on('logout', () => {
+                this.$router.push('/logout')
+            })
+        }
     }
-  }
-}
 </script>
