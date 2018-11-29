1121225cebcc08a81ba01c455435c5ff864ac492
Negar <n.jamalifard@gmail.com>
2018-11-30 00:34:50 +0330

    clean up home component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,13 @@
 <template>
-    <HelloWorld/>
+
 </template>
 
 <script>
-    import HelloWorld from '../components/HelloWorld'
 
     export default {
         components: {
-            HelloWorld
+
         },
-        created(){
-            this.$electron.ipcRenderer.on('logout', () => {
-                this.$router.push('/logout')
-            })
-        }
+
     }
 </script>
