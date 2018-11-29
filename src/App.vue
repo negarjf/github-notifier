aa9b0adc17ad09a117b54918861d4fbbf36c7594
Negar <n.jamalifard@gmail.com>
2018-11-30 00:32:26 +0330

    add bottom nav

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,34 @@
 
     <v-content>
       <router-view/>
+        <v-bottom-nav
+                :active.sync="bottomNav"
+                color="primary"
+                :value="true"
+                app
+                fixed
+                shift
+        >
+            <v-btn dark>
+                <span>Video</span>
+                <v-icon>ondemand_video</v-icon>
+            </v-btn>
+
+            <v-btn dark>
+                <span>Music</span>
+                <v-icon>music_note</v-icon>
+            </v-btn>
+
+            <v-btn dark>
+                <span>Book</span>
+                <v-icon>book</v-icon>
+            </v-btn>
+
+            <v-btn dark>
+                <span>Image</span>
+                <v-icon>image</v-icon>
+            </v-btn>
+        </v-bottom-nav>
     </v-content>
   </v-app>
 </template>
